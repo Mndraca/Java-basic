3a7f7d595651057914b780fcc97462d12f923b7f
Mndraca <marizanovic3@gmail.com>
2024-01-10 10:29:37 -0500

    main

---
+++ index.js
@@ -0,0 +1,12 @@
+  //This is my first JS code
+  console.log('Hallo its me')
+  let name = 'Milena';
+  console.log(name);
+  let interestRate = 0.3;
+  interestRate = 1;
+  console.log(interestRate);
+  let person = {
+    name: 'Milena',
+    age: 40
+  };
+  console.log(person);
